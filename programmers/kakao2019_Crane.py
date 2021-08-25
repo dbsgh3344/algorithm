e5130f1bb11704fc50f8afe3fefd652862782db0
Yunho <dbsgh3344@naver.com>
2021-08-25 22:31:50 +0900

    test

--- programmers/kakao2019_Crane.py
+++ programmers/kakao2019_Crane.py
@@ -1,31 +1,39 @@
+from collections import deque
 
 def solution1(boards, moves) :
     basket = []
+    moves = deque(moves)
     cnt = 0
 
-    for move in moves :
-        while(boards[move-1]):
-            catch_doll =boards[move-1].pop()
-            if catch_doll!=0:
+    while(moves) :
+        cur_doll =0
+        for i in range(len(boards)) :        
+            if boards[i][moves[0]-1] !=0 :                                
+                cur_doll=boards[i][moves[0]-1]
+                boards[i][moves[0]-1] = 0                     
                 break
-                              
-        if catch_doll!=0:
-            basket.append(catch_doll)
+        
 
-        if len(basket)>1 and (basket[-2] == basket[-1]) :
-            basket.pop()
+        moves.popleft()
+            
+        if (len(basket)>=1) and (basket[-1]==cur_doll):
             basket.pop()
             cnt+=2
-        catch_doll=0
-
+        else :
+            if(cur_doll!=0) :
+                basket.append(cur_doll)
+        
+                
     return cnt
 
 
 
-
-
 if __name__== '__main__' :
-    b= [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
+    b= [[0,0,0,0,0],
+        [0,0,1,0,3],
+        [0,2,5,0,1],
+        [4,2,4,4,2],
+        [3,5,1,3,1]]
     m = [1,5,3,5,1,2,1,4]
     a= solution1(b,m)
     print(a)
