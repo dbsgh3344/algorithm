7914e1c630fed86ddd30c6577731c160d0d98a6b
Yunho <dbsgh3344@naver.com>
2021-08-26 00:19:32 +0900

    retouching algorithm

--- programmers/kakao2020_compressionChar.py
+++ programmers/kakao2020_compressionChar.py
@@ -1,12 +1,39 @@
-import numpy as np 
-
 
 def solution(s) :
-    print("1")
-    return 1
+    tmp = len(s)
+    for i in range(len(s)) :
+        answer_chars=''
+        iter_chars= s[:1+i]
+
+        cnt=1
+        for j in range(1+i,len(s)+1,i+1) :
+            if iter_chars == s[j:1+i+j] :
+                cnt+=1
+            else :
+                if cnt>1:
+                    answer_chars+= str(cnt)+iter_chars                
+                else :
+                    answer_chars+=iter_chars
+                
+                iter_chars= s[j:1+i+j]
+                cnt=1
+                continue
+            
+            # if (j>=len(s)-1) :
+            #     if cnt>1:
+            #         answer_chars+= str(cnt)+iter_chars                
+            #     else :
+            #         answer_chars+=iter_chars
+                
+        print(answer_chars)        
+        if 0< len(answer_chars) < tmp :
+            tmp = len(answer_chars)
     
+    return tmp
+            
 
 
 if __name__=="__main__":
     s= "aabbaccc"
-    solution(s)
+    ans= solution(s)
+    print(ans)
