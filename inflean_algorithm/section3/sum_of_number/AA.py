3801f14d4a48d5b3c5226e23876b92e3ae2f017c
Yunho <dbsgh3344@naver.com>
2022-05-11 23:21:01 +0900

    success sum of number

--- inflean_algorithm/section3/sum_of_number/AA.py
+++ inflean_algorithm/section3/sum_of_number/AA.py
@@ -4,22 +4,43 @@ from itertools import combinations
 
 n,m = map(int,input().split())
 numlist = list(map(int,input().split()))
-
 answer = 0
 
 
-for i in range(len(numlist)):
-    cnt = numlist[i]    
-    for j in range(i+1,len(numlist)) :
-        if cnt ==m :
-            break
-        elif cnt > m:
-            break
-        else :
-            cnt+=numlist[j]
+# for i in range(len(numlist)):
+#     cnt = numlist[i]    
+#     for j in range(i+1,len(numlist)) :
+#         if cnt ==m :
+#             break
+#         elif cnt > m:
+#             break
+#         else :
+#             cnt+=numlist[j]
     
-    if cnt ==m :
+#     if cnt ==m :
+#         answer+=1
+
+# print(answer)
+    
+
+lt = 0
+rt = 1
+tot = numlist[lt]
+
+while lt!=len(numlist)-1 :
+    if tot < m:
+        if rt >= len(numlist) :
+            break
+        tot+=numlist[rt]
+        rt+=1
+    elif tot ==m :
         answer+=1
+        tot-=numlist[lt]
+        lt+=1
+    else :
+        tot-=numlist[lt]
+        lt+=1
+
+
 
 print(answer)
-    
