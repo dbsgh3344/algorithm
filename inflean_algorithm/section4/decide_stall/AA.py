bd83e19def04bc6be407838571ed8598a3a83d71
Yunho <dbsgh3344@naver.com>
2022-05-24 23:36:41 +0900

    unsolved decide stall

--- inflean_algorithm/section4/decide_stall/AA.py
+++ inflean_algorithm/section4/decide_stall/AA.py
@@ -1,3 +1,20 @@
 import sys
 sys.stdin = open('inputs.txt','r')
 
+n,c = map(int,input().split())
+arr = sorted([int(input()) for _ in range(n)])
+
+lt =0
+rt = n-1
+thres = (rt+lt)//2
+res= 234234
+while lt<=rt :
+    left = abs(arr[lt]-arr[thres])
+    right = abs(arr[rt]-arr[thres])
+    minimum= min(left,right)
+    # if left > right :
+        
+    
+    if res > minimum:
+        res = minimum
+    
