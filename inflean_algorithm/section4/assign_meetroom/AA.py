f677a4b475880aa6aefc1729e9ed6fa0005318a4
Yunho <dbsgh3344@naver.com>
2022-05-28 00:40:06 +0900

    update unsolved meetroom and have to find method to reducing time

--- inflean_algorithm/section4/assign_meetroom/AA.py
+++ inflean_algorithm/section4/assign_meetroom/AA.py
@@ -1,27 +1,24 @@
 import sys
-# sys.stdin = open('inputs.txt','r')
+sys.stdin = open('inputs.txt','r')
 
 n= int(input())
 arr= [tuple(map(int,input().split())) for _ in range(n)]
-sortarr= sorted(arr,key=lambda x:x[0])
+sortarr= sorted(arr,key=lambda x:x[1])
 idx=0
 res = 0
 while idx!=n :
     cnt =1
-    base =sortarr[idx]
     start = sortarr[idx][0]
     end =sortarr[idx][1]
-
-    for i in range(n) :
-        if i==idx:
+    
+    for i in range(idx+1,n) :
+        if end > sortarr[i][0] :
             continue
         else :
-            if end >sortarr[i][0] :
-                continue
-            else :
-                start = sortarr[i][0]
-                end = sortarr[i][1]
-                cnt+=1 
+            start = sortarr[i][0]
+            end = sortarr[i][1]
+            cnt+=1
+
     
     if res <cnt :
         res = cnt
