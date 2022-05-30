4f08c57e8eb3f156d382e04d0933bef9a301d22c
Yunho <dbsgh3344@naver.com>
2022-05-30 20:45:42 +0900

    solved meetroom

--- inflean_algorithm/section4/assign_meetroom/AA.py
+++ inflean_algorithm/section4/assign_meetroom/AA.py
@@ -1,29 +1,16 @@
 import sys
-sys.stdin = open('inputs.txt','r')
+# sys.stdin = open('inputs.txt','r')
 
 n= int(input())
 arr= [tuple(map(int,input().split())) for _ in range(n)]
 sortarr= sorted(arr,key=lambda x:x[1])
-idx=0
-res = 0
-while idx!=n :
-    cnt =1
-    start = sortarr[idx][0]
-    end =sortarr[idx][1]
-    
-    for i in range(idx+1,n) :
-        if end > sortarr[i][0] :
-            continue
-        else :
-            start = sortarr[i][0]
-            end = sortarr[i][1]
-            cnt+=1
+et = 0
+cnt = 0
+for s,e in sortarr :
+    if s >= et :
+        et = e
+        cnt+=1
+        
 
-    
-    if res <cnt :
-        res = cnt
-
-    idx+=1
-    
-print(res)
+print(cnt)
     
