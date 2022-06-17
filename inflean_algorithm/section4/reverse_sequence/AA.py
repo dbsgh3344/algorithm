85f1b0bdb266dd24766f2a9cdb008fdb2d5d71a1
Yunho <dbsgh3344@naver.com>
2022-06-18 00:01:36 +0900

    solved max number

--- inflean_algorithm/section4/reverse_sequence/AA.py
+++ inflean_algorithm/section4/reverse_sequence/AA.py
@@ -1,5 +1,5 @@
 import sys
-from collections import deque
+import time
 sys.stdin = open('inputs.txt','r')
 
 n= int(input())
@@ -7,10 +7,29 @@ arr= list(map(int,input().split()))
 arr= arr[::-1]
 lists = []
 
+# 초기방법
+st= time.time()
 for idx,num in enumerate(arr) :
     idx= len(arr)-idx
     lists.insert(num,str(idx))
 
+print(time.time()-st)
+# 초기방법 시간 0초
+
+# 정석
+# st = time.time()
+# for i in range(n) :
+#     cnt=0
+        
+#     # lists[idx+1]=i+1
+#     for j in range(len(lists)) :
+#         if cnt==arr[i] and lists[j]==0:
+#             lists[j]=str(i+1)
+#             break
+#         if lists[j]==0:
+#             cnt+=1
+# print(time.time()-st)
+# 정석 방법 시간 0.0010027885437011719초
 print(' '.join(lists))
 
 
