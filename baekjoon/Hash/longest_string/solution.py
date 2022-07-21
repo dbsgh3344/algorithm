1c492c65127a355eb705c6319b45a8580a19400a
dbsgh3344 <dbsgh3344@naver.com>
2022-07-21 22:06:56 +0900

    solved stack 1

---
+++ baekjoon/Hash/longest_string/solution.py
@@ -0,0 +1,29 @@
+
+### 시간초과 unsolved
+import sys
+sys.stdin = open('inputs.txt','r')
+
+n= int(input())
+strs = input()
+idx= 2
+hashdicts = {}
+while idx!=(len(strs)//2)+1:
+    for i in range(len(strs)-idx+1):
+        
+        tmp=strs[i:i+idx]
+        if tmp in hashdicts.keys():
+            hashdicts[tmp]+=1
+        else:
+            hashdicts[tmp]=1
+    idx+=1
+
+# print(hashdicts)
+ans =0
+for k,v in hashdicts.items():
+    if v>=2:
+        if ans <len(k):
+            ans= len(k)
+
+print(ans)
+
+        
--- baekjoon/Stack/editor/solution.py
+++ baekjoon/Stack/editor/solution.py
@@ -1,17 +1,22 @@
+# sys.stdin=open('inputs.txt','r')
+
 import sys
-sys.stdin=open('inputs.txt','r')
 
-arr= input()
-m = int(input())
-# orders= [ for _ in range(m)]
-
-cursor = len(arr)
-for i in range(m):
-    order = input()
-    if order =='L':
-        cursor -=1
-    elif order =='D':
-        cursor+=1
+arr= list(sys.stdin.readline().strip())
+stash =[]
+for _ in range(int(sys.stdin.readline())):
+    order = sys.stdin.readline().strip()
+    if order =='L' and len(arr)!=0:
+        stash.append(arr.pop())
+    elif order =='D' and len(stash)!=0:
+        arr.append(stash.pop())
     elif 'P' in order:
-        
+        _,char = order.split()
+        arr.append(char)
+    elif order == 'B' and len(arr)!=0:
+        arr.pop()
+
+# for _ in range(len(stash)): arr.append(stash.pop())
+arr.extend(reversed(stash))
+print(''.join(arr))
     
