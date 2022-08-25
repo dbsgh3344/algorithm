d37ae02f14de5b8ca6b4be5c332e2cabad538339
dbsgh3344 <dbsgh3344@naver.com>
2022-08-26 00:22:19 +0900

    solved goodfriend but reference

--- baekjoon/Queue/GoodFriend/solution.py
+++ baekjoon/Queue/GoodFriend/solution.py
@@ -1,24 +1,50 @@
 import sys
 from collections import deque
-# sys.stdin = open('inputs.txt','r')
+sys.stdin = open('inputs.txt','r')
 
 n,k = map(int,sys.stdin.readline().split())
 names = deque([sys.stdin.readline().strip() for _ in range(n)])
-deq = deque([])
+
+# --------------------- 시간초과 코드
+# deq = deque([])
+# cnt=deque([0])
+# idx= 0
+# ans=0
+
+# deq.append(names.popleft())
+# # while len(deq)!=0:
+# while len(names)!=0:
+#     if idx==k:
+#         ans+=cnt.popleft()
+#         deq.popleft()
+#         idx=0
+
+#     # if len(names)!=0:
+#     next = names.popleft()
+#     for i in range(len(deq)):
+#         if len(deq[i])==len(next) :
+#             cnt[i]+=1
+    
+#     deq.append(next)
+#     cnt.append(0)
+#     # else :
+#     #     break
+
+#     idx+=1
+
+# ans+=sum(cnt)
+# print(ans) 
+    
+
+q= list([deque() for _ in range(21)])
 cnt=0
-idx= 0
-deq.append(names.popleft())
-while len(names)!=1:
-    if len(deq)>k:
-        deq.popleft()
-    
-    if len(deq)>1:
-        if len(deq[0])==len(deq[-1]) :
-            cnt+=1
-    
+for i in range(n):
+    name_len = len(names[i])-1
 
-    deq.append(names.popleft())
-        
-
+    while q[name_len] and i-q[name_len][0]>k:
+        q[name_len].popleft()
     
+    cnt+=len(q[name_len])
+    q[name_len].append(i)
+
 print(cnt)
