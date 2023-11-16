6dcd127c69e393dddceff0e4e6371ca346a1e982
Yunho Song <dbsgh3322@gmail.com>
2023-11-16 17:43:39 +0900

    fail solution 2 retry

--- programmers/level2/int_btw_2_circles.py
+++ programmers/level2/int_btw_2_circles.py
@@ -1,22 +1,34 @@
-
-
-
+from collections import deque
 def solution(r1,r2) :
     cnt = 0
-    for i in range(r2+1) :        
-        for j in range(r2+1) :
-            r = (i**2 + j**2)**0.5
-            if r >= r1 and r <= r2 :
-                cnt+=1
-                # print(i,j)
-                
-    cnt*=4
-    cnt-=8 
     
-    return cnt
+    lists= []
+    for x in range(r2+1) :
+        y = (r2**2-x**2)**0.5
+        for i in range(int(y//1)+1) :
+            lists.append((x,i))
     
 
+    lists2= deque()
+    for x in range(r1+1) :
+        y = (r1**2 - x**2)**0.5
+        # print(x,y)
+        for i in range(int(y//1)+1) :
+            lists2.append((x,i))
 
+    while lists2 :
+        a= lists2.popleft() 
+        if a in lists :
+            lists.remove(a)
+        else :
+            lists2.append(a)
+        
 
-r1,r2 = 2,3
+    print(lists)
+    # cnt+=8 
+    
+    return len(lists)
+    
+
+r1,r2 = 2,5
 print(solution(r1,r2))
